8b3466469137585662ff0d2086dea32dc9d4278f
Peter Njuguna <njugunapm23@gmail.com>
2022-09-28 20:49:08 +0300

    added the brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 import "./brand.css";
 
-const brand = () => {
-  return <div>brand</div>;
+const Brand = () => {
+  return (
+    <div className="gpt3__brand section__padding">
+      <div className="">
+        <img src={google} alt="google" />
+      </div>
+      <div className="">
+        <img src={slack} alt="slack" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
-export default brand;
+export default Brand;
